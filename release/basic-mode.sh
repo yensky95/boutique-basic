71036ec70a04f96382759f670c0278f27de8445d
Simone Dell'Oro <dello.1995@hotmail.it>
2022-03-21 20:56:08 +0100

    another fix

--- release/basic-mode.sh
+++ release/basic-mode.sh
@@ -3,18 +3,16 @@
 adservice=$(kubectl get deployment adservice)
 recommendationservice=$(kubectl get deployment recommendationservice)
 
-if [ -z "$adservice"]
+if [ -n "$adservice"]
 then
     kubectl delete deployment adservice 
     kubectl delete svc adservice
-    .
 fi
 
-if [ -z "$recommendationservice"]
+if [ -n "$recommendationservice"]
 then
     kubectl delete deployment recommendationservice 
     kubectl delete svc recommendationservice
-    .
 fi
 
-kubectl apply -f kubernetes-manifest-basic
+kubectl apply -f kubernetes-manifest-basic.yaml
