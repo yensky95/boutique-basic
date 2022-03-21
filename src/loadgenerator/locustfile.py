56235f22bfaae16bb55576db8c621526dd3eab1a
Simone Dell'Oro <dello.1995@hotmail.it>
2022-03-21 23:37:16 +0100

    generators

--- src/loadgenerator/locustfile.py
+++ src/loadgenerator/locustfile.py
@@ -19,13 +19,10 @@ from locust import HttpUser, TaskSet, between
 
 products = [
     '0PUK6V6EV0',
-    '1YMWWN1N4O',
     '2ZYFJ3GM2N',
-    '66VCHSJNUP',
     '6E92ZMYYFZ',
     '9SIQT8TOJO',
     'L9ECAV7KIM',
-    'LS4PSXUNUM',
     'OLJCESPC7Z']
 
 def index(l):
