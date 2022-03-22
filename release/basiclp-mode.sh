b1461084235bc4e12ac6982150f6b3ce4f936217
Simone Dell'Oro <dello.1995@hotmail.it>
2022-03-22 09:25:51 +0100

    bash basic low power

---
+++ release/basiclp-mode.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+adservice=$(kubectl get deployment adservice)
+recommendationservice=$(kubectl get deployment recommendationservice)
+
+if [ -n "$adservice" ]
+then
+    kubectl delete deployment adservice 
+    kubectl delete svc adservice
+fi
+
+if [ -n "$recommendationservice" ]
+then
+    kubectl delete deployment recommendationservice 
+    kubectl delete svc recommendationservice
+fi
+
+kubectl apply -f kubernetes-manifests-basiclp.yaml
