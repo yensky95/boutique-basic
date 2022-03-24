a948f2ee8059f84df64719f448703e349a6c2ed6
Simone Dell'Oro <dello.1995@hotmail.it>
2022-03-24 23:33:18 +0100

    last fix

--- release/autodeploy.sh
+++ release/autodeploy.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
+#TO RUN
+#prometheus deployment running on http://localhost:9090 and being inside /boutique-basic/release/ folder 
 
 echo "Deploying normal mode ..."
 bash normal-mode-hp.sh
 sleep 15
 
 
+#execmode: 0 -> normal high performance, 1 -> normal low power , 2 -> basic high performance, 3 -> basic low power
+declare -i execmode=0
+
 while true 
 do
-
-    #execmode: 0 -> normal high performance, 1 -> normal low power , 2 -> basic high performance, 3 -> basic low power
-    declare -i execmode=0
 
     content=$( curl -g 'http://localhost:9090/api/v1/query?query=rate(istio_request_duration_milliseconds_sum{app="frontend",source_app="loadgenerator",response_code="200"}[1m])/rate(istio_request_duration_milliseconds_count{app="frontend",source_app="loadgenerator",response_code="200"}[1m])' )
     sleep 2
@@ -17,6 +19,8 @@ do
     valuea=$(echo $value | cut -c 2-)
     valuefd=$(echo $valuea | awk '{printf "%d", $1}')
     toint=$(($valuefd+0))
+
+    echo "Latency value: $toint"
 
     if [[ $toint -gt 100 && $execmode -eq 0 ]]
     then
@@ -64,7 +68,7 @@ do
         sleep 15
     fi
 
-    echo "Next data fetched in 30 seconds ... execute mode $execmode. Current latency $toint"
+    echo "Next data fetched in 30 seconds ... execute mode $execmode."
     sleep 30
 
 done
