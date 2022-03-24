a5b889dea43bd496dca63de9ba32c1c874cf1875
Simone Dell'Oro <dello.1995@hotmail.it>
2022-03-24 22:52:53 +0100

    fix

--- release/autodeploy.sh
+++ release/autodeploy.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+bash normal-mode-hp.sh
+sleep 15
+echo "Deploying normal mode ..."
+
 while true 
 do
 
     #execmode: 0 -> normal high performance, 1 -> normal low power , 2 -> basic high performance, 3 -> basic low power
     declare -i execmode=0
 
-    content=$( curl -g 'http://localhost:9090/api/v1/query?query=rate(istio_request_duration_milliseconds_sum{app="frontend",source_app="loadgenerator",response_code="200"}[2m])/rate(istio_request_duration_milliseconds_count{app="frontend",source_app="loadgenerator",response_code="200"}[2m])' )
+    content=$( curl -g 'http://localhost:9090/api/v1/query?query=rate(istio_request_duration_milliseconds_sum{app="frontend",source_app="loadgenerator",response_code="200"}[1m])/rate(istio_request_duration_milliseconds_count{app="frontend",source_app="loadgenerator",response_code="200"}[1m])' )
     sleep 2
     value=$( jq '.data.result[].value[1]' <<< "${content}" )
     valuea=$(echo $value | cut -c 2-)
