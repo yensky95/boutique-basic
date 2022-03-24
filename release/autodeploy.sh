ea342297e4638bf53be61b33e311a2beb19fc8a0
Simone Dell'Oro <dello.1995@hotmail.it>
2022-03-24 22:28:51 +0100

    fix2

--- release/autodeploy.sh
+++ release/autodeploy.sh
@@ -12,23 +12,23 @@ do
     valuefd=$(echo $valuef | awk '{printf "%d", $1}')
     toint=$(($valuefd+0))
 
-    if [ $toint -gt 100 && execmode==0 ]
+    echo $toint
+
+    if [ $toint -gt 100 && execmode==0 ];
     then
         echo "Going into normal low power mode"
         bash normal-mode-lp.sh
         execmode=1
-    else if [ $toint -gt 100 && execmode==1 ]
-        then    
-            echo "Going into basic high performance mode"
-            bash basichp-mode.sh
-            execmode=2
-        else if [ $toint -gt 100 ]
-            then   
-                echo "Going into basic low power mode"
-                bash basiclp-mode.sh
-                execmode=3
-            fi
-        fi
+    elif [ $toint -gt 100 && execmode==1 ];
+    then    
+        echo "Going into basic high performance mode"
+        bash basichp-mode.sh
+        execmode=2
+    elif [ $toint -gt 100 ];
+    then   
+        echo "Going into basic low power mode"
+        bash basiclp-mode.sh
+        execmode=3
     fi
 
     echo "Next data fetched in 30 seconds ... execute mode $execmode. Current latency $toint"
